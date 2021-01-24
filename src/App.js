cbf2bdcddb2be810d93f562c81bc4bd21c3c8479
sina_riazi <sina.riyazi@gmail.com>
2021-01-24 12:32:47 +0100

    #update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.comonent';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
