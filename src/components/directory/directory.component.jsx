8816b657e87ce267d2fce82d7ac1ac85f5f21ed6
sina_riazi <sina.riyazi@gmail.com>
2021-04-05 10:36:08 +0200

    homepage pics updated

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,32 +12,32 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/crwn-db-b2d79.appspot.com/o/DSC_3220.JPG?alt=media&token=73187425-9982-4c0c-a4f9-6bb96046cc06',
           id: 1,
           linkUrl: 'hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/crwn-db-b2d79.appspot.com/o/DSC_8602-2.png?alt=media&token=20980153-821a-40c5-ad65-27d5c80a1d1a',
           id: 2,
           linkUrl: ''
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/crwn-db-b2d79.appspot.com/o/DSC_8754-2.png?alt=media&token=221db92f-f6f0-4953-bd6b-66aa00f14cdbhttps://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
           linkUrl: ''
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/crwn-db-b2d79.appspot.com/o/Dirt%20Run%20-%202019%20Salzburg%20-%20094.jpg?alt=media&token=b46b5cf7-264c-4259-8de2-ea5a8704201e',
           size: 'large',
           id: 4,
           linkUrl: ''
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/crwn-db-b2d79.appspot.com/o/EL3A4067.jpg?alt=media&token=cf295762-5ebd-400c-8207-1462d883e4d6',
           size: 'large',
           id: 5,
           linkUrl: ''
